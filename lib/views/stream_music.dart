3d3aa17ad3377f4bfeda0e73b909eb806bf92623
NguyenDucTuanUET <64391055+ductuanUET@users.noreply.github.com>
2022-09-24 11:10:18 +0700

    add stream music view

---
+++ lib/views/stream_music.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StreamMusicView extends StatefulWidget {
+  const StreamMusicView({super.key});
+
+  @override
+  State<StreamMusicView> createState() => _StreamMusicViewState();
+}
+
+class _StreamMusicViewState extends State<StreamMusicView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
