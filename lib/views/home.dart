301aeeb312da11114eba6dc40950ea2105cd920b
NguyenDucTuanUET <64391055+ductuanUET@users.noreply.github.com>
2022-09-27 13:01:56 +0700

    add more listview and update scrollable home view

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -28,172 +28,324 @@ class _HomeViewState extends State<HomeView> {
                     color: Colors.grey[600], size: 30)),
           ],
         ),
-        body: Column(
-          children: [Container(
-            //backgroundColor: Colors.green,
-            color: Colors.white,
-            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'More of what you like',
-                  style: TextStyle(
-                    fontSize: 25,
-                    color: Colors.black87,
-                    fontFamily: 'Interstate',
-                  ),
-                ),
-                Text(
-                  'Suggestions based on what you\'ve liked or played',
-                  style: TextStyle(
-                    fontSize: 15,
-                    color: Colors.grey[600],
-                    fontFamily: 'Interstate',
-                  ),
-                ),
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: [
-                      SizedBox(
-                        height: 140,
-                        width: 140,
-                        child: Card(
-                          child: Image(image: AssetImage('assets/album1.jpg')),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 140,
-                        width: 140,
-                        child: Card(
-                          child: Image(image: AssetImage('assets/album2.jpg')),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 140,
-                        width: 140,
-                        child: Card(
-                          child: Image(image: AssetImage('assets/album3.jpg')),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 140,
-                        width: 140,
-                        child: Card(
-                          child: Image(image: AssetImage('assets/album4.jpg')),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 140,
-                        width: 140,
-                        child: Card(
-                          child: Image(image: AssetImage('assets/album5.jpg')),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 140,
-                        width: 140,
-                        child: Card(
-                          child: Image(image: AssetImage('assets/album6.jpg')),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 140,
-                        width: 140,
-                        child: Card(
-                          child: Image(image: AssetImage('assets/album7.jpg')),
-                        ),
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
+        body: Stack(
+          children: [
+            Container(
+            width: double.infinity,
+            height: double.infinity,
+            color: Colors.red,
           ),
-          Container(
-            //backgroundColor: Colors.green,
-            color: Colors.white,
-            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+            SingleChildScrollView(
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Recently Played',
-                  style: TextStyle(
-                    fontSize: 25,
-                    color: Colors.black87,
-                    fontFamily: 'Interstate',
-                  ),
+              children: [Container(
+                //backgroundColor: Colors.green,
+                color: Colors.white,
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'More of what you like',
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.black87,
+                        fontFamily: 'Interstate',
+                      ),
+                    ),
+                    Text(
+                      'Suggestions based on what you\'ve liked or played',
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.grey[600],
+                        fontFamily: 'Interstate',
+                      ),
+                    ),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          SizedBox(
+                            height: 140,
+                            width: 140,
+                            child: Card(
+                              child: Image(image: AssetImage('assets/album1.jpg')),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 140,
+                            width: 140,
+                            child: Card(
+                              child: Image(image: AssetImage('assets/album2.jpg')),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 140,
+                            width: 140,
+                            child: Card(
+                              child: Image(image: AssetImage('assets/album3.jpg')),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 140,
+                            width: 140,
+                            child: Card(
+                              child: Image(image: AssetImage('assets/album4.jpg')),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 140,
+                            width: 140,
+                            child: Card(
+                              child: Image(image: AssetImage('assets/album5.jpg')),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 140,
+                            width: 140,
+                            child: Card(
+                              child: Image(image: AssetImage('assets/album6.jpg')),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 140,
+                            width: 140,
+                            child: Card(
+                              child: Image(image: AssetImage('assets/album7.jpg')),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
                 ),
-                // Text(
-                //   'Suggestions based on what you\'ve liked or played',
-                //   style: TextStyle(
-                //     fontSize: 15,
-                //     color: Colors.grey[600],
-                //     fontFamily: 'Interstate',
-                //   ),
-                // ),
-                SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: [
-                      SizedBox(
-                        height: 140,
-                        width: 140,
-                        child: Card(
-                          child: Image(image: AssetImage('assets/album10.jpg')),
-                        ),
+              ),
+              Container(
+                //backgroundColor: Colors.green,
+                color: Colors.white,
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Recently Played',
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.black87,
+                        fontFamily: 'Interstate',
                       ),
-                      SizedBox(
-                        height: 140,
-                        width: 140,
-                        child: Card(
-                          child: Image(image: AssetImage('assets/album9.jpg')),
-                        ),
+                    ),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          SizedBox(
+                            height: 140,
+                            width: 140,
+                            child: Card(
+                              child: Image(image: AssetImage('assets/album10.jpg')),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 140,
+                            width: 140,
+                            child: Card(
+                              child: Image(image: AssetImage('assets/album9.jpg')),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 140,
+                            width: 140,
+                            child: Card(
+                              child: Image(image: AssetImage('assets/album8.jpg')),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 140,
+                            width: 140,
+                            child: Card(
+                              child: Image(image: AssetImage('assets/album4.jpg')),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 140,
+                            width: 140,
+                            child: Card(
+                              child: Image(image: AssetImage('assets/album7.jpg')),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 140,
+                            width: 140,
+                            child: Card(
+                              child: Image(image: AssetImage('assets/album6.jpg')),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 140,
+                            width: 140,
+                            child: Card(
+                              child: Image(image: AssetImage('assets/album12.jpg')),
+                            ),
+                          ),
+                        ],
                       ),
-                      SizedBox(
-                        height: 140,
-                        width: 140,
-                        child: Card(
-                          child: Image(image: AssetImage('assets/album8.jpg')),
-                        ),
+                    )
+                  ],
+                ),
+              ),
+          
+              Container(
+                //backgroundColor: Colors.green,
+                color: Colors.white,
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Chill',
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.black87,
+                        fontFamily: 'Interstate',
                       ),
-                      SizedBox(
-                        height: 140,
-                        width: 140,
-                        child: Card(
-                          child: Image(image: AssetImage('assets/album4.jpg')),
-                        ),
+                    ),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          SizedBox(
+                            height: 140,
+                            width: 140,
+                            child: Card(
+                              child: Image(image: AssetImage('assets/album5.jpg')),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 140,
+                            width: 140,
+                            child: Card(
+                              child: Image(image: AssetImage('assets/album7.jpg')),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 140,
+                            width: 140,
+                            child: Card(
+                              child: Image(image: AssetImage('assets/album3.jpg')),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 140,
+                            width: 140,
+                            child: Card(
+                              child: Image(image: AssetImage('assets/album4.jpg')),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 140,
+                            width: 140,
+                            child: Card(
+                              child: Image(image: AssetImage('assets/album8.jpg')),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 140,
+                            width: 140,
+                            child: Card(
+                              child: Image(image: AssetImage('assets/album6.jpg')),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 140,
+                            width: 140,
+                            child: Card(
+                              child: Image(image: AssetImage('assets/album12.jpg')),
+                            ),
+                          ),
+                        ],
                       ),
-                      SizedBox(
-                        height: 140,
-                        width: 140,
-                        child: Card(
-                          child: Image(image: AssetImage('assets/album7.jpg')),
-                        ),
+                    )
+                  ],
+                ),
+              ),
+          
+              Container(
+                //backgroundColor: Colors.green,
+                color: Colors.white,
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Workout',
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.black87,
+                        fontFamily: 'Interstate',
                       ),
-                      SizedBox(
-                        height: 140,
-                        width: 140,
-                        child: Card(
-                          child: Image(image: AssetImage('assets/album6.jpg')),
-                        ),
+                    ),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          SizedBox(
+                            height: 140,
+                            width: 140,
+                            child: Card(
+                              child: Image(image: AssetImage('assets/album13.jpg')),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 140,
+                            width: 140,
+                            child: Card(
+                              child: Image(image: AssetImage('assets/top50.jpg')),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 140,
+                            width: 140,
+                            child: Card(
+                              child: Image(image: AssetImage('assets/album5.jpg')),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 140,
+                            width: 140,
+                            child: Card(
+                              child: Image(image: AssetImage('assets/album4.jpg')),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 140,
+                            width: 140,
+                            child: Card(
+                              child: Image(image: AssetImage('assets/album7.jpg')),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 140,
+                            width: 140,
+                            child: Card(
+                              child: Image(image: AssetImage('assets/album6.jpg')),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 140,
+                            width: 140,
+                            child: Card(
+                              child: Image(image: AssetImage('assets/album12.jpg')),
+                            ),
+                          ),
+                        ],
                       ),
-                      SizedBox(
-                        height: 140,
-                        width: 140,
-                        child: Card(
-                          child: Image(image: AssetImage('assets/album12.jpg')),
-                        ),
-                      ),
-                    ],
-                  ),
-                )
-              ],
+                    )
+                  ],
+                ),
+              ),
+              ] 
             ),
-          ),
-          ] 
+          ),] 
         ),
         
         );
