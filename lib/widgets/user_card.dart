af964c3678632fdee6340b25656fd4d7aa3a698c
NguyenDucTuanUET <64391055+ductuanUET@users.noreply.github.com>
2022-10-02 13:59:11 +0700

    update clickable follow button

--- lib/widgets/user_card.dart
+++ lib/widgets/user_card.dart
@@ -1,47 +1,78 @@
 import 'package:flutter/material.dart';
-import '../views/profilePage/profile.dart';
+import '../views/userPage/user.dart';
 
-class UserCard extends StatelessWidget {
+class UserCard extends StatefulWidget {
   final ImageProvider? image;
   final String? userName;
-  final int? id;
   final Function? onTap;
+  bool isFollowed = false;
 
-  const UserCard({super.key, this.image, this.userName, this.onTap, this.id});
+  UserCard({super.key, this.image, this.userName, this.onTap});
 
   @override
+  State<UserCard> createState() => _UserCardState();
+}
+
+class _UserCardState extends State<UserCard> {
+  @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => ProfileView(
-              image: image,
-              id: id,
-            ),
-          ),
-        );
-      },
-      child: Column(
-        children: [
-          Container(
-            padding: const EdgeInsets.only(bottom: 5),
-            child: CircleAvatar(
-              backgroundColor: Colors.grey,
-              radius: 61,
-              child: CircleAvatar(
-                backgroundImage: image,
-                radius: 60,
+    return Column(
+      children: [
+        GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => UserView(
+                  image: widget.image,
+                ),
               ),
-            ),
+            );
+          },
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.only(bottom: 5),
+                child: CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  radius: 61,
+                  child: CircleAvatar(
+                    backgroundImage: widget.image,
+                    radius: 60,
+                  ),
+                ),
+              ),
+              Container(
+                  padding: EdgeInsets.all(3),
+                  child: Text(widget.userName!,
+                      style: TextStyle(color: Colors.black, fontSize: 16))),
+            ],
           ),
-          Container(
-              padding: EdgeInsets.all(3),
-              child: Text(userName!,
-                  style: TextStyle(color: Colors.black, fontSize: 16))),
-        ],
-      ),
+        ),
+        Container( 
+            child: (widget.isFollowed == false)
+                ? ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        widget.isFollowed = !widget.isFollowed; 
+                      });
+                    },
+                    child: Text('Follow',
+                        style: TextStyle(color: Colors.white, fontSize: 14)),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.orange[900],
+                    ))
+                : TextButton(
+                    onPressed: () {
+                      setState(() {
+                        widget.isFollowed = !widget.isFollowed; 
+                      });
+                    },
+                    child: Text('Following',
+                        style:
+                            TextStyle(color: Colors.grey[600], fontSize: 14)),
+                  )),
+      ],
     );
   }
 }
