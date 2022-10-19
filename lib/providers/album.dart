5b27d902023b25f52cbe71b16c88fde0658ad14d
NguyenDucTuanUET <64391055+ductuanUET@users.noreply.github.com>
2022-10-20 00:40:04 +0700

    add album provider

---
+++ lib/providers/album.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/foundation.dart';
+
+class Album with ChangeNotifier {
+  final String? id;
+  final String? title;
+  final String? description;
+  final String? imageUrl;
+  bool? isFavorite = false;
+
+  Album({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.imageUrl,
+    this.isFavorite = false,
+  });
+}
