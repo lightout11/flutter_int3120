7fedb4a52db960d39eb98caa3a8f15a2b4f8abe0
NguyenDucTuanUET <64391055+ductuanUET@users.noreply.github.com>
2022-10-20 00:41:14 +0700

    update album item

--- lib/widgets/album_item.dart
+++ lib/widgets/album_item.dart
@@ -1,32 +1,36 @@
 import "package:flutter/material.dart";
+import 'package:learnflutter/providers/albums.dart';
+import 'package:provider/provider.dart';
+import '../providers/album.dart';
 import '../screens/album_screens/album_view.dart';
 
-class AlbumCard extends StatelessWidget {
-  final ImageProvider? image;
-  final String? albumName;
-  final String? albumNote;
-  final Function? onTap;
-  final double? width;
-  final double? height;
-  const AlbumCard(
-      {Key? key,
-      this.image,
-      this.albumName,
-      this.albumNote,
-      this.onTap,
-      this.width,
-      this.height})
-      : super(key: key);
+class AlbumItem extends StatelessWidget {
+  // final ImageProvider? image;
+  // final String? albumName;
+  // final String? albumNote;
+  // final Function? onTap;
+  // final double? width;
+  // final double? height;
+  // const AlbumCard(
+  //     {Key? key,
+  //     this.image,
+  //     this.albumName,
+  //     this.albumNote,
+  //     this.onTap,
+  //     this.width,
+  //     this.height})
+  //     : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final loadedAlbum = Provider.of<Album>(context);
     return GestureDetector(
       onTap: () {
         Navigator.push(
           context,
           MaterialPageRoute(
             builder: (context) => AlbumView(
-              image: image,
+              imageUrl: loadedAlbum.imageUrl,
             ),
           ),
         );
@@ -35,28 +39,28 @@ class AlbumCard extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           SizedBox(
-            height: height,
-            width: width,
+            height: 180,
+            width: 140,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Container(
                   padding: const EdgeInsets.only(right: 5, bottom: 5),
                   child: Card(
-                    child: Image(image: image!),
+                    child: Image.network(loadedAlbum.imageUrl!),
                   ),
                 ),
                 Container(
                   padding: const EdgeInsets.only(left: 5, right: 5, bottom: 5),
                   child: Text(
-                    albumName!,
+                    loadedAlbum.title!,
                     style: const TextStyle(color: Colors.black),
                   ),
                 ),
                 Container(
                   padding: const EdgeInsets.only(left: 5, right: 5),
                   child: Text(
-                    albumNote!,
+                    loadedAlbum.description!,
                     style: const TextStyle(color: Colors.grey),
                   ),
                 ),
