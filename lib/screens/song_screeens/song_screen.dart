0569fb40755c355ed07642166b7724d2422efd99
NguyenDucTuanUET <64391055+ductuanUET@users.noreply.github.com>
2022-10-31 21:32:59 +0700

    Add song screen

---
+++ lib/screens/song_screeens/song_screen.dart
@@ -0,0 +1,110 @@
+//import 'package:assets_audio_player/assets_audio_player.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:learnflutter/widgets/audio_file.dart';
+//import 'package:just_audio/just_audio.dart';
+//import 'package:rxdart/rxdart.dart' as rxdart;
+import '../../models/song_model.dart';
+import '../../widgets/seek_bar.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+
+class SongScreen extends StatefulWidget {
+  const SongScreen({super.key});
+
+  @override
+  State<SongScreen> createState() => _SongScreenState();
+}
+
+class _SongScreenState extends State<SongScreen> with SingleTickerProviderStateMixin {
+  Song song = Get.arguments ?? Song.songs[0];
+  AudioPlayer? advancedPlayer;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    advancedPlayer = AudioPlayer();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        extendBodyBehindAppBar: true,
+        body: Stack(
+          fit: StackFit.expand,
+          children: [
+            Image.network(
+              song.coverUrl,
+              fit: BoxFit.cover,
+            ),
+            const _BackgroundFilter(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 40.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    song.title,
+                    style: Theme.of(context).textTheme.headlineSmall!.copyWith(color: Colors.white, fontWeight: FontWeight.bold,),
+                  ),
+                  SizedBox(height: 10,),
+                  Text(
+                    song.description,
+                    maxLines: 2,
+                    style: Theme.of(context).textTheme.bodySmall!.copyWith(color: Colors.white),
+                  ),
+                  SizedBox(height: 30,),
+                  AudioFile(advancedPlayer: advancedPlayer!,),
+                  
+                ],
+              ),
+            )
+          ],
+        ));
+  }
+}
+
+class _BackgroundFilter extends StatelessWidget {
+  const _BackgroundFilter({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ShaderMask(
+      shaderCallback: (rect) {
+        return LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.white,
+              Colors.white.withOpacity(0.5),
+              Colors.white.withOpacity(0.0),
+            ],
+            stops: [
+              0.0,
+              0.4,
+              0.6
+            ]).createShader(rect);
+      },
+      blendMode: BlendMode.dstOut,
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.deepOrange.shade200,
+              Colors.deepOrange.shade800,
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
