bbc7e57b05cdae88e95edd07a6587f6a88c2c995
NguyenDucTuanUET <64391055+ductuanUET@users.noreply.github.com>
2022-11-16 22:41:24 +0700

    update profile view

--- lib/screens/profile_screens/profile.dart
+++ lib/screens/profile_screens/profile.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'components/profile_menu.dart';
+import 'components/profile_pic.dart';
+
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key});
 
@@ -25,8 +28,39 @@ class ProfileView extends StatelessWidget {
           title: const Text('Profile'),
         ),
         body: SingleChildScrollView(
-          child: Center(child: Text('Profile here!'),)
-        ),
+      padding: EdgeInsets.symmetric(vertical: 20),
+      child: Column(
+        children: [
+          ProfilePic(),
+          SizedBox(height: 20),
+          ProfileMenu(
+            text: "My Account",
+            icon: "assets/icons/User Icon.svg",
+            press: () => {},
+          ),
+          ProfileMenu(
+            text: "Notifications",
+            icon: "assets/icons/Bell.svg",
+            press: () {},
+          ),
+          ProfileMenu(
+            text: "Settings",
+            icon: "assets/icons/Settings.svg",
+            press: () {},
+          ),
+          ProfileMenu(
+            text: "Help Center",
+            icon: "assets/icons/Question mark.svg",
+            press: () {},
+          ),
+          ProfileMenu(
+            text: "Log Out",
+            icon: "assets/icons/Log out.svg",
+            press: () {},
+          ),
+        ],
+      ),
+    ),
       ),
     );
   }
